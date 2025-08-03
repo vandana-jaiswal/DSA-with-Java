ab5c10b82ca50645d0bdcceaed558d5b7d7f5454
vandana-jaiswal <vandanajaiswal546@gmail.com>
2025-08-03 10:48:07 +0530

    LinkedList problem solve

---
+++ day_12/Example.java
@@ -0,0 +1,45 @@
+//  leetcode problem solve q1
+class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode head;
+
+    public ListNode(int data) {
+        
+        this.val = data;
+        this.next = null;
+    }
+
+    ListNode() {
+
+    }
+}
+
+public class Example {
+    public ListNode addTwoNumbers(new ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        int carry = 0;
+        
+        while(l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            
+            int sum = x + y + carry;
+            int digit = sum % 10;
+            carry = sum / 0;
+            
+            
+            temp.next = new ListNode(digit);
+            temp = temp.next;
+        
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    
+}
---
+++ day_12/index.java
@@ -0,0 +1,22 @@
+// doubly linked list
+public class index {
+    public static void main(String[] args) {
+        Node obj = new Node();
+        // obj.addLastNode(6);
+        // obj.display();
+        // System.out.println("------------");
+        // obj.addLastNode(8);
+        // obj.display();
+
+        obj.addFirstNode(5);
+        obj.display();
+        System.out.println("------");
+        obj.addFirstNode(6);
+        obj.display();
+         System.out.println("------");
+        obj.addFirstNode(3);
+        obj.display();
+         System.out.println("------");
+
+    }
+}
